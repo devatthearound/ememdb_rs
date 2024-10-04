49c1d988393677edab937ac74a25529be65a4c5b
devatthearound <dev@the-around.com>
2024-10-04 15:58:44 +0900

    Document

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod config;
 pub mod subscription;
 
 // Re-export key items to make them accessible from outside the library
-pub use db::{InMemoryDB, OperationResult,
+pub use db::{InMemoryDB, OperationResult,Document,
 Collection};            // Now users can access InMemoryDB from the root
 pub use query::{QueryBuilder, JoinBuilder};       // Now users can access Query from the root
 pub use config::{TTL, KeyType, CollectionConfig};     // Re-export multiple items from config
